bcd77a2d7e18d25fc04cf593d697d7587bcb8b18
Renato Pacheco <renato.pacheco@outlook.com>
2022-02-17 14:11:06 -0300

    Instalando ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputTextModule } from '@app/shared/components';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { InputTextModule } from '@app/shared/components';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    InputTextModule
+    InputTextModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
